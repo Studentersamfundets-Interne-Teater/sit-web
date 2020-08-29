8210afbc76f3f3223b83aafad8ed239eb53c1fb8
fjeldis <fjeldberghelena@gmail.com>
2020-08-29 16:25:08 +0200

    Started creating hamburger menu

---
+++ SITnett/graphics/hamburger.js
@@ -0,0 +1,8 @@
+function hamburgerFunction() {
+    var x = document.getElementById("menuLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
