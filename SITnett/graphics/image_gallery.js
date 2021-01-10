7a698ca6bb03bcec8f29de962901f91c9088fe44
Live <livejac@gmail.com>
2021-01-10 14:56:47 +0100

    Utkast til galleri

--- SITnett/graphics/image_gallery.js
+++ SITnett/graphics/image_gallery.js
@@ -4,3 +4,24 @@ function galleryNavigation(imgs) {
     expandImg.src = imgs.src;
     imgText.innerHTML = imgs.alt;
   }
+
+let imageList = document.getElementsByClassName("gallery-nav");
+  
+//hent data
+//rendre data
+/*
+function renderImageList(data) {
+  let html = '';
+
+  for (let i = 0; i < data.length; i++) {
+      const img = data[i];
+      html += `
+        <figure>
+          <img src="${img.url}" />
+          <figcaption>${img.type}</figcaption>
+        </figure>
+      `;
+  }
+
+  return html;
+}*/
