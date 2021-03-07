306fc697af570d3b040086d595087239133670f5
flarrek <jonas@solgard.no>
2021-03-08 00:01:36 +0100

    Fix display of 'navn' in /kontakt/.

--- SITnett/SITnett/settings.py
+++ SITnett/SITnett/settings.py
@@ -96,7 +96,7 @@ class FEATURE_SETTINGS():
     TOGGLE_UTTRYKK = False  # skrur på uttrykksliste.
     TOGGLE_ARKIV = False  # skrur på arkivsida (med tidslinje over produksjoner, arrangementer, bilder og hendelser).
     TOGGLE_DOKUMENTER = False  # skrur på dokumentfunksjonalitet.
-    TOGGLE_EDIT = False  # skrur på redigeringsfunksjonalitet.
+    TOGGLE_EDIT = True  # skrur på redigeringsfunksjonalitet.
 
 
 FEATURES = FEATURE_SETTINGS()
